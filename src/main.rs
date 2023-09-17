28c64b5bec6086738998434d6ae8d0f2a3edc475
winterbloom <23458874+winterbloom@users.noreply.github.com>
2023-09-16 23:21:09 -0700

    Draw crosses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use druid::widget::{Label, Flex, LensWrap, Painter};
-use druid::{AppLauncher, Widget, WindowDesc, Data, Lens, WidgetExt, LensExt, RenderContext, Color};
+use druid::widget::{Label, Flex, LensWrap, Painter, AspectRatioBox};
+use druid::{AppLauncher, Widget, WindowDesc, Data, Lens, WidgetExt, RenderContext, Color};
 use druid::im::Vector;
 use druid::piet::kurbo::Line;
 
 const WINDOW_SIZE: (f64, f64) = (600.0, 400.0);
 const BOARD_SIZE: usize = 9;
+const LINE_WEIGHT: f64 = 5.0;
 
 #[derive(Clone, Data, Lens)]
 struct GameState {
@@ -19,8 +20,6 @@ struct Board {
 #[derive(Clone, Data)]
 struct BoardPoint {
     owner: Option<Player>,
-    i: usize,
-    j: usize,
 }
 
 #[derive(Clone, Data, PartialEq)]
@@ -28,23 +27,30 @@ enum Player {
     Black, White
 }
 
-fn build_point_ui() -> impl Widget<BoardPoint> {
-    let painter = Painter::new(|ctx, data: &BoardPoint, _| {
+fn build_point_ui(i: usize, j: usize) -> impl Widget<Board> {
+    let painter = Painter::new(|ctx, data: &Board, _| {
         let bounds = ctx.size().to_rect();
+        let mid_x = (bounds.x0 + bounds.x1) / 2.0;
+        let mid_y = (bounds.y0 + bounds.y1) / 2.0;
+        // Vertical line
         ctx.stroke(
-            Line::new((bounds.x0, bounds.y0), (bounds.x1, bounds.y1)),
-            &Color::rgb8(0, 0, 0), 10.0)
+            Line::new((mid_x, bounds.y0), (mid_x, bounds.y1)),
+            &Color::BLACK, LINE_WEIGHT
+        );
+        // Horizontal line
+        ctx.stroke(
+            Line::new((bounds.x0, mid_y), (bounds.x1, mid_y)),
+            &Color::BLACK, LINE_WEIGHT
+        );
     });
-    Label::new("x").center().background(painter).expand()
+    Label::new("x").center().background(painter)
 }
 
 // Constructs a given row's UI
 fn build_row_ui(i: usize) -> impl Widget<Board> {
     let mut row_ui = Flex::row();
     for j in 0..BOARD_SIZE {
-        row_ui = row_ui.with_flex_child(
-            LensWrap::new(build_point_ui(), Board::board.index(i).index(j)
-        ), 1.0);
+        row_ui = row_ui.with_child(build_point_ui(i, j));
     }
     row_ui
 }
@@ -53,7 +59,7 @@ fn build_row_ui(i: usize) -> impl Widget<Board> {
 fn build_board_ui() -> impl Widget<Board> {
     let mut board_ui = Flex::column();
     for i in 0..BOARD_SIZE {
-        board_ui = board_ui.with_flex_child(build_row_ui(i), 1.0);
+        board_ui = board_ui.with_child(build_row_ui(i));
     }
     board_ui
 }
@@ -66,10 +72,10 @@ fn build_ui() -> impl Widget<GameState> {
 // Creates an empty board
 fn board_init() -> Board {
     let mut board = Vector::new();
-    for i in 0..BOARD_SIZE {
+    for _ in 0..BOARD_SIZE {
         let mut row = Vector::new();
-        for j in 0..BOARD_SIZE {
-            row.push_front(BoardPoint { owner: None, i: i, j: j } );
+        for _ in 0..BOARD_SIZE {
+            row.push_front(BoardPoint { owner: None } );
         }
         board.push_front(row);
     }
