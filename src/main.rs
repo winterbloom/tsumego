ef404ff3763c4c05a19b53dbbaab9bd674b4cb36
winterbloom <23458874+winterbloom@users.noreply.github.com>
2023-09-16 22:36:27 -0700

    Start basic UI for board

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,88 @@
-use druid::widget::Label;
-use druid::{AppLauncher, Widget, WindowDesc};
+use druid::widget::{Label, Flex, LensWrap, Painter};
+use druid::{AppLauncher, Widget, WindowDesc, Data, Lens, WidgetExt, LensExt, RenderContext, Color};
+use druid::im::Vector;
+use druid::piet::kurbo::Line;
 
-fn build_ui() -> impl Widget<()> {
-    Label::new("Hello world")
+const WINDOW_SIZE: (f64, f64) = (600.0, 400.0);
+const BOARD_SIZE: usize = 9;
+
+#[derive(Clone, Data, Lens)]
+struct GameState {
+    board: Board,
+}
+
+#[derive(Clone, Data, Lens)]
+struct Board {
+    board: Vector<Vector<BoardPoint>>,
+}
+
+#[derive(Clone, Data)]
+struct BoardPoint {
+    owner: Option<Player>,
+    i: usize,
+    j: usize,
+}
+
+#[derive(Clone, Data, PartialEq)]
+enum Player {
+    Black, White
+}
+
+fn build_point_ui() -> impl Widget<BoardPoint> {
+    let painter = Painter::new(|ctx, data: &BoardPoint, _| {
+        let bounds = ctx.size().to_rect();
+        ctx.stroke(
+            Line::new((bounds.x0, bounds.y0), (bounds.x1, bounds.y1)),
+            &Color::rgb8(0, 0, 0), 10.0)
+    });
+    Label::new("x").center().background(painter).expand()
+}
+
+// Constructs a given row's UI
+fn build_row_ui(i: usize) -> impl Widget<Board> {
+    let mut row_ui = Flex::row();
+    for j in 0..BOARD_SIZE {
+        row_ui = row_ui.with_flex_child(
+            LensWrap::new(build_point_ui(), Board::board.index(i).index(j)
+        ), 1.0);
+    }
+    row_ui
+}
+
+// Constructs the board's UI, with its rows
+fn build_board_ui() -> impl Widget<Board> {
+    let mut board_ui = Flex::column();
+    for i in 0..BOARD_SIZE {
+        board_ui = board_ui.with_flex_child(build_row_ui(i), 1.0);
+    }
+    board_ui
+}
+
+// Constructs the entire UI
+fn build_ui() -> impl Widget<GameState> {
+    LensWrap::new(build_board_ui(), GameState::board)
+}
+
+// Creates an empty board
+fn board_init() -> Board {
+    let mut board = Vector::new();
+    for i in 0..BOARD_SIZE {
+        let mut row = Vector::new();
+        for j in 0..BOARD_SIZE {
+            row.push_front(BoardPoint { owner: None, i: i, j: j } );
+        }
+        board.push_front(row);
+    }
+    Board { board: board }
 }
 
 fn main() {
-    let main_window = WindowDesc::new(build_ui())
-        .window_size((600.0, 400.0))
-        .title("My first Druid App");
-    let initial_data = ();
+    let win = WindowDesc::new(build_ui())
+        .window_size(WINDOW_SIZE)
+        .title("Tsumego");
+    let initial_state = GameState { board : board_init() };
 
-    AppLauncher::with_window(main_window)
-        .launch(initial_data)
+    AppLauncher::with_window(win)
+        .launch(initial_state)
         .expect("Failed to launch application");
 }
